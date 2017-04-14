1e62ddc5f0a8fa6f71952736138ee764633743bc
Brad Hain <bradhain@gmail.com>
2017-04-14 10:16:25 -0600

    checkout page

--- src/components/checkout/CheckOut.js
+++ src/components/checkout/CheckOut.js
@@ -3,50 +3,66 @@ import pluralize from "pluralize";
 import RaisedButton from "material-ui/RaisedButton";
 import {Link} from "react-router";
 import "./CheckOut";
+import Storage from "../localStorage.js";
 
 const style = {
     margin: 12
 };
 
-class CheckOut extends Component{
+class CheckOut extends Component {
 
+    constructor() {
+        super()
+        this.state = {cart: Storage.get("CART")};
+        console.log(this.state);
+    }
 
-    render(){
-
-        const quantity= this.props.quantity + "" + pluralize("1", this.props.quantity);
-        const subtotal= this.props.quantity * this.prop.price;
-        const tax= Math.floor((subtotal / 100)) * this.props.tax;
-        const total= subtotal + tax;
-
-        return(
-            <div className="CheckOut">
-                <div className="Title">Check Out</div>
+    render() {
+        console.log(this.state);
+        return (
+            <div>
                 <table>
-                    <tbody>
+                    <thead>
                     <tr>
-                        <td>{this.props.price} x {quantity}</td>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Subtotal</th>
                     </tr>
+                    </thead>
+
+                    {
+
+                        this.state.cart.map(item => (
+                            <tr key={item._id}>
+                                <td>{item.pottype}</td>
+                                <td>{item.price}</td>
+                                <td>{item.price * item.quantity}</td>
+                            </tr>
+                        ))
+
+
+                    }
+
+                    <tfoot>
                     <tr>
-                        <td>Subtotal</td>
-                        <td>{subtotal} $</td>
+                        <td>Total</td>
+                        <td colSpan="2" style={{ textAlign: 'right'}}>
+                            {this.state.cart.map(item => item.price * item.quantity).reduce((a, b) => {
+                                return a + b;
+                            }, 0)}
+                        </td>
                     </tr>
-                    <tr>
-                        <td>Tax</td>
-                        <td>{tax} $</td>
-                    </tr>
-                    </tbody>
+                    </tfoot>
+
                 </table>
-                <div className="Total">
-                    <div className="TotalLabel">Total</div>
-                    <div className="Amount">
-                        {total} <small>$</small>
-                    </div>
-                    <Link to="home"> <RaisedButton label="Home" style={style}></RaisedButton></Link>
-                </div>
+
+
             </div>
         )
+
     }
-};
+}
+;
 
 export default CheckOut;
 
